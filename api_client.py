f6c6d23c427509d6b2e50559ca964304e92a1dfd
Paul Dumoulin <paul.l.dumoulin@gmail.com>
2017-02-24 03:53:24 +0000

    first version of search script

--- api_client.py
+++ api_client.py
@@ -12,22 +12,66 @@ class StubHubAPIClient(object):
             self.access_token = config.get('access_token', None)
             self.endpoint = config.get('endpoint', None)
 
-    def get_events(self, params={}):
+    def get_events(self, search, params={}):
+        params['q'] = search
         params['sort'] = 'eventDateLocal'
-        return self._call(
+        params['rows'] = 500
+        params['parking'] = False
+        (code, results) = self._call(
             'GET', 
             'search/catalog/events/v3', 
             params
         )
+        if code != 200:
+            raise Exception('non-200 response on get_events() : %s' % code)
+
+        events = []
+        for event in results['events']:
+            event_dict = {
+                'id'        : event['id'],
+                'name'      : event['name'],
+                'date'      : event['eventDateLocal'],
+                'status'    : event['status'].lower(),
+                'url'       : 'https://www.stubhub.com/%s' % event['webURI']
+            }
+            extra_attributes = ['act_primary', 'act_secondary']
+            for attr in extra_attributes:
+                event_dict[attr] = ''
+            for attr in event['attributes']:
+                if attr['name'] in extra_attributes:
+                    event_dict[attr['name']] = attr['value'].lower()
+            events.append(event_dict)
+        return events
 
     # needed to request special access apisupport@stubhub.com
-    def get_listings(self, event_id, params={}):
+    def get_listings(self, event_id, quantity, max_price, params={}):
         params['eventid'] = event_id
-        return self._call(
+        params['sort'] = 'currentprice'
+        params['quantity'] = quantity
+        params['priceMax'] = max_price
+        (code, results) = self._call(
             'GET',
             'search/inventory/v2',
             params
         )
+        if code != 200:
+            raise Exception('non-200 response on get_listings() : %s' % code)
+        tickets = []
+        for listing in results.get('listing', []):
+            categories = listing.get('listingAttributeCategoryList', [])
+            tickets.append({
+                'id'         : listing['listingId'],
+                'full_price' : listing['currentPrice']['amount'],
+                'list_price' : listing['listingPrice']['amount'],
+                'quantity'   : listing['quantity'],
+                'row'        : listing['row'],
+                'seats'      : listing['seatNumbers'].split(','),
+                'section'    : listing['sectionName'],
+                'obstructed' : 1 in categories,
+                'piggy_back' : 5 in categories
+            })
+            pass
+        return tickets
 
     def _call(self, verb, route, params, auth=True):
         headers  = { 'Accept' : 'application/json' }
---
+++ search.py
@@ -0,0 +1,63 @@
+
+import argparse
+
+from pprint import pformat
+from api_client import StubHubAPIClient
+c = StubHubAPIClient('conf/conf.json')
+
+# TODO - handle 429s gracefully
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-q', required=True, type=int, help='number of tickets to search for')
+    parser.add_argument('-p', required=True, type=float, help='max price per ticket in dollars and cents')
+    parser.add_argument('-l', required=False, type=int, default=None, help='limit number of events')
+    parser.add_argument('--home', required=False, type=str, default='', help='home team name')
+    parser.add_argument('--away', required=False, type=str, default='', help='away team name')
+    parser.add_argument('-d', required=False, type=str, help='date of game (YYYY-MM-DD)')
+    args = parser.parse_args()
+    
+    # variable args
+    quantity = args.q
+    max_price = args.p
+    home_team = args.home.lower()
+    away_team = args.away.lower()
+    event_limit = args.l
+    date = args.d
+
+    # static filtering args
+    event_status = 'active'
+    obstructed = False
+    piggy_back = False
+
+    # get events based on home and away team name
+    search = ("%s %s" % (home_team, away_team)).title()
+    events = c.get_events(search)
+    events = filter(lambda e: e['status'] == event_status, events)
+
+    # filter out false positives from search term
+    if home_team != '':
+        events = filter(lambda e: e['act_primary'] == home_team, events)
+    if away_team != '':
+        events = filter(lambda e: e['act_secondary'] == away_team, events)
+
+    # limit results to minimize ticket fetch requests
+    if event_limit is not None:
+        events = events[0:event_limit]
+
+    # go through events to find tickets
+    for event in events:
+        tickets = c.get_listings(event['id'], quantity, max_price)
+        # TODO - print event header
+        if not obstructed:
+            tickets = filter(lambda t: t['obstructed'] == True, tickets)
+        if not piggy_back:
+            tickets = filter(lambda t: t['piggy_back'] == True, tickets)
+        for ticket in tickets:
+            # TODO - print some data
+            pass
+
+if __name__ == '__main__':
+    main()
+
+
